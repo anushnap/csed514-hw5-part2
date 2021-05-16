35ad79a1ba20d6bfadb37a17143a9af34c819761
Dwight Sablan <sabland@uw.edu>
2021-05-15 19:34:31 -0700

    add vaccine patient file w/ init method

---
+++ VaccinePatient.py
@@ -0,0 +1,36 @@
+import pymssql
+
+class VaccinePatient:
+	'''Adds patient to Database'''
+	def __init__(self,
+                 patient_name,
+                 vaccine_status,
+                 cursor):
+
+		self.sqltext = "INSERT INTO Patients ("
+        self.sqltext += "PatientName, VaccineStatus) VALUES ("
+        self.sqltext += "'" + patient_name + "', "
+        self.sqltext += str(vaccine_status) + ")"
+
+        self.patientId = 0
+
+        try: 
+            cursor.execute(self.sqltext)
+            cursor.connection.commit()
+            cursor.execute("SELECT @@IDENTITY AS 'Identity'; ")
+            _identityRow = cursor.fetchone()
+            self.patientId = _identityRow['Identity']
+            # cursor.connection.commit()
+            print('Query executed successfully. Patient : ' + patient_name
+            +  ' added to the database using Patient ID = ' + str(self.patientId))
+        except pymssql.Error as db_err:
+            print("Database Programming Error in SQL Query processing for Patients! ")
+            print("Exception code: " + str(db_err.args[0]))
+            if len(db_err.args) > 1:
+                print("Exception message: " + db_err.args[1])
+            print("SQL text that resulted in an Error: " + self.sqltext)
+
+        def ReserveAppointment():
+
+        def Schedule Appointment():
+        	
