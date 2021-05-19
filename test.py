f29ad7809acfa55814eacd43374ec6f0bd54c84a
anushnap <anushnap@uw.edu>
2021-05-18 23:02:12 -0700

    deleted comments

--- test.py
+++ test.py
@@ -86,37 +86,5 @@ class TestVaccineCaregiver(unittest.TestCase):
                     clear_tables(sqlClient)
                     self.fail("CareGiverSchedule verification failed")
 
-# class TestCovid19(unittest.TestCase):
-#     def test_init(self):
-#         with SqlConnectionManager(Server=os.getenv("Server"),
-#                                   DBname=os.getenv("DBName"),
-#                                   UserId=os.getenv("UserID"),
-#                                   Password=os.getenv("Password")) as sqlClient:
-#             with sqlClient.cursor(as_dict=True) as cursor:
-#                 try:
-#                     # clear the tables before testing
-#                     clear_tables(sqlClient)
-#                     # create a new VaccineCaregiver object
-#                     self.vaccine_a = covid(manufacName="Moderna",
-#                                            days_between_doses = 28,
-#                                            cursor=cursor)
-#                     # check if the patient is correctly inserted into the database
-#                     sqlQuery = '''
-#                                SELECT *
-#                                FROM Vaccines
-#                                WHERE ManufactererName = 'Moderna'
-#                                '''
-#                     cursor.execute(sqlQuery)
-#                     rows = cursor.fetchall()
-#                     if len(rows) < 1:
-#                         self.fail("Creating COVID vaccine failed")
-#                     # clear the tables after testing, just in-case
-#                     clear_tables(sqlClient)
-#                 except Exception:
-#                     # clear the tables if an exception occurred
-#                     clear_tables(sqlClient)
-#                     self.fail("Creating COVID vaccine failed")
-
-
 if __name__ == '__main__':
     unittest.main()
